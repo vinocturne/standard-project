4a00e9143841a50520ac226bb6fe95a51c83d27c
ilsan4509 <ilsan4509@naver.com>
2020-12-17 10:52:16 +0900

    김성곤_대기회원테이블VO생성_WaitingCustomerVO

---
+++ src/main/java/org/standard/vo/WaitingCustomerVO.java
@@ -0,0 +1,8 @@
+package org.standard.vo;
+
+import java.sql.Date;
+
+public class WaitingCustomerVO {
+    private String wc_Id, wc_Password, wc_Name, wc_Address1, wc_Address2, wc_Phone1, wc_Phone2, businessNumber, wc_Email, role, brandName;
+    private Date wc_Birthday, wc_RegisterDay;
+}
