f1c813e754b5d929f3b3a81c2bc84da683c7c942
taehun <xun415@naver.com>
2020-12-21 22:56:14 +0900

    태훈 - common패키지에 암호화, Email 클래스 작성

---
+++ src/main/java/org/standard/project/common/Encrypt.java
@@ -0,0 +1,33 @@
+package org.standard.project.common;
+
+import java.security.MessageDigest;
+
+public class Encrypt {
+
+	public String encrypt(String plainText) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(plainText.getBytes());
+			byte byteData[] = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for(int i=0; i<byteData.length;i++) {
+				
+				sb.append(Integer.toString((byteData[i]&0xff)+0x100,16).substring(1));
+				
+			}
+			StringBuffer hexString = new StringBuffer();
+			for(int i=0; i<byteData.length;i++) {
+				String hex = Integer.toHexString(0xff &byteData[i]);
+				if(hex.length()==1) {
+					hexString.append(hex);
+				}
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		throw new RuntimeException();
+	}
+
+}
