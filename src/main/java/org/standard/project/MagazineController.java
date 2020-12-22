d6484df846cc3afbe4841bb47f2b08b68d967be9
damlee89 <damlee89@gmail.com>
2020-12-23 02:54:18 +0900

    담 테스트시도해보다 실패 다시 돌려놓기

--- src/main/java/org/standard/project/MagazineController.java
+++ src/main/java/org/standard/project/MagazineController.java
@@ -1,12 +1,17 @@
 package org.standard.project;
 
+import java.util.Locale;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.standard.project.customer.CustomerService;
 
 @Controller
 public class MagazineController {
+	
 
 }
