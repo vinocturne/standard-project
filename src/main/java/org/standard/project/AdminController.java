e00e658f893d2d27c506462c0c9159dc367e5c28
vinocturne <vinocturne@naver.com>
2020-12-24 15:27:17 +0900

    12/24 신건희 admin페이지 연동

---
+++ src/main/java/org/standard/project/AdminController.java
@@ -0,0 +1,15 @@
+package org.standard.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value = "/Admin")
+public class AdminController {
+	@RequestMapping(value = "/AdminMain", method = RequestMethod.GET)
+	public void adminMain() {
+		
+	}
+}
